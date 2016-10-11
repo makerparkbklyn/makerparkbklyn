442ab29188be72790b3548a5c78c4bdc0296a9cf
mattarnoldpentagram <matt.arnold@pentagram.com>
2016-10-11 18:54:32 -0400

    hook up form to MP_Form google sheet

--- js/form.js
+++ js/form.js
@@ -1,36 +1,60 @@
-function validateEmail(email) {
-        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-};
+$(document).ready(function() {
 
-function postToGoogle() {
-    var name = $j('#firstname').val();
-	var name = $j('#lastname').val();
-    var email = $j('#email').val();
-    if ((name !== "") && (email !== "") && ((feed !== "") && (validateEmail(email)))) {
-        $j.ajax({
-            url: "https://goo.gl/forms/RdO948g8JVgBTV8b2",
-            data: {"entry.1" : firstname, "entry.2" : lastname, "entry.2" : email},
-            type: "POST",
-            dataType: "xml",
-            statusCode: {
-                0: function (){
+	// Variable to hold request
+	var request;
 
-                    $j('#firstname').val("");
-					$j('#lastname').val("");
-                    $j('#email').val("");
-                    //Success message
-                },
-                200: function (){
-					$j('#firstname').val("");
-					$j('#lastname').val("");
-                    $j('#email').val("");
-                    //Success Message
-                }
-            }
-        });
-    }
-    else {
-        //Error message
-    }
-};
+	// Bind to the submit event of our form
+	$("#join-form").submit(function(event){
+
+		// Abort any pending request
+		if (request) {
+			request.abort();
+		}
+		// setup some local variables
+		var $form = $(this);
+
+		// Let's select and cache all the fields
+		var $inputs = $form.find("input");
+
+		// Serialize the data in the form
+		var serializedData = $form.serialize();
+
+		// Let's disable the inputs for the duration of the Ajax request.
+		// Note: we disable elements AFTER the form data has been serialized.
+		// Disabled form elements will not be serialized.
+		$inputs.prop("disabled", true);
+
+		// Fire off the request to /form.php
+		request = $.ajax({
+			url: "https://script.google.com/macros/s/AKfycbzYtw3ii8QbglDAeM157b6tMEPfX1GbBD5fBxaAdRmOtBDpGaHr/exec",
+			type: "post",
+			data: serializedData
+		});
+
+		// Callback handler that will be called on success
+		request.done(function (response, textStatus, jqXHR){
+			// Log a message to the console
+			console.log("Hooray, it worked!");
+			console.log(response);
+			console.log(textStatus);
+			console.log(jqXHR);
+		});
+
+		// Callback handler that will be called on failure
+		request.fail(function (jqXHR, textStatus, errorThrown){
+			// Log the error to the console
+			console.error("The following error occurred: "+ textStatus, errorThrown);
+		});
+
+		// Callback handler that will be called regardless
+		// if the request failed or succeeded
+		request.always(function () {
+			// Reenable the inputs
+			$inputs.prop("disabled", false);
+		});
+
+		// Prevent default posting of form
+		event.preventDefault();
+	});
+
+});
