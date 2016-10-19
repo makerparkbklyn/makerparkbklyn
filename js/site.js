27fe6d83cac138748e4bdad6c39e756bdccfac12
mattarnoldpentagram <matt.arnold@pentagram.com>
2016-10-19 15:36:56 -0400

    rename arrow to arrow-down; rework site.js so that right rail comes in and out on news page

--- js/site.js
+++ js/site.js
@@ -47,7 +47,7 @@ $(document).on('ready', function() {
 	}
 	// -------------------------------------------------------------------------
 	// animate smooth scroll to next section on arrow click
-	$('body').on('click', '.arrow', function(e) {
+	$('body').on('click', '.arrow-down', function(e) {
 		e.preventDefault();
 		var $next = $currentSection.next('section');
 		if (!$next.length > 0) {
@@ -63,9 +63,65 @@ $(document).on('ready', function() {
 
 	$(window).scroll(function() {
 		var $scroll = $(window).scrollTop();
-		var $currentTop = $currentSection.offset().top;
-		var $currentBottom = $currentSection.offset().top + $currentSection.outerHeight();
-		var $currentTitle = $currentSection.find('.title');
+
+		// front page
+		if ($('.front-page').length > 0) {
+			var $currentTop = $currentSection.offset().top;
+			var $currentBottom = $currentSection.offset().top + $currentSection.outerHeight();
+			var $currentTitle = $currentSection.find('.title');
+			// ---------------------------------------------------------------------
+			// set currentSection
+			if ($currentSection.next('section').length > 0) {
+				var $nextTop = $currentSection.next('section').offset().top;
+			}
+			if ($scroll > ($nextTop - ($(window).height()/2))) {
+				$currentSection = $currentSection.next('section');
+				$currentTop = $currentSection.offset().top;
+				$currentBottom = $currentSection.offset().top + $currentSection.outerHeight();
+				$currentTitle = $currentSection.find('.title');
+				$('section.current').removeClass('current');
+				$currentSection.addClass('current');
+			}
+			if ($scroll < ($currentTop - ($(window).height()/2))) {
+				$currentSection = $currentSection.prev('section');
+				$currentTop = $currentSection.offset().top;
+				$currentBottom = $currentSection.offset().top + $currentSection.outerHeight();
+				$currentTitle = $currentSection.find('.title');
+				$('section.current').removeClass('current');
+				$currentSection.addClass('current');
+			}
+			// ---------------------------------------------------------------------
+			// make titles stick
+			if ($scroll >= ($currentTop - 160)) {
+				$('section').find('.title').removeClass('stuck');
+				$currentTitle.removeClass('top');
+				$currentTitle.addClass('stuck');
+
+			} else if ($scroll < ($currentTop - 160)) {
+				$('section').find('.title').removeClass('stuck');
+				$currentTitle.removeClass('stuck');
+				$currentTitle.addClass('top');
+			}
+			if ($scroll >= ($currentBottom - $(window).height() + 160)) {
+				$('section').find('.title').removeClass('stuck');
+				$currentTitle.removeClass('stuck');
+				$currentTitle.addClass('bottom');
+			}
+			else if ($scroll < ($currentBottom - $(window).height() + 160) && $scroll >= ($currentTop - 160)) {
+				$('section').find('.title').removeClass('stuck');
+				$currentTitle.removeClass('bottom');
+				$currentTitle.addClass('stuck');
+			}
+			// ---------------------------------------------------------------------
+			// show and hide logo (right rail), scrolling from hero
+			var $missionTop = $('.mission-section').offset().top;
+			if ($scroll > ($missionTop - .6*$(window).height())) {
+				$('.logo').removeClass('out');
+			}
+			else if ($scroll < ($missionTop - .6*$(window).height())) {
+				$('.logo').addClass('out');
+			}
+		}
 		// ---------------------------------------------------------------------
 		// hide rails on downscroll
 		var st = this.pageYOffset || this.scrollTop;
@@ -83,67 +139,13 @@ $(document).on('ready', function() {
 		}
 		lastScrollTop = st;
 		// ---------------------------------------------------------------------
-		// set currentSection
-		if ($currentSection.next('section').length > 0) {
-			var $nextTop = $currentSection.next('section').offset().top;
-		}
-		if ($scroll > ($nextTop - ($(window).height()/2))) {
-			$currentSection = $currentSection.next('section');
-			$currentTop = $currentSection.offset().top;
-			$currentBottom = $currentSection.offset().top + $currentSection.outerHeight();
-			$currentTitle = $currentSection.find('.title');
-			$('section.current').removeClass('current');
-			$currentSection.addClass('current');
-		}
-		if ($scroll < ($currentTop - ($(window).height()/2))) {
-			$currentSection = $currentSection.prev('section');
-			$currentTop = $currentSection.offset().top;
-			$currentBottom = $currentSection.offset().top + $currentSection.outerHeight();
-			$currentTitle = $currentSection.find('.title');
-			$('section.current').removeClass('current');
-			$currentSection.addClass('current');
-		}
-
-		// ---------------------------------------------------------------------
-		// make titles stick
-		if ($scroll >= ($currentTop - 160)) {
-			$('section').find('.title').removeClass('stuck');
-			$currentTitle.removeClass('top');
-			$currentTitle.addClass('stuck');
-
-		} else if ($scroll < ($currentTop - 160)) {
-			$('section').find('.title').removeClass('stuck');
-			$currentTitle.removeClass('stuck');
-			$currentTitle.addClass('top');
-		}
-		if ($scroll >= ($currentBottom - $(window).height() + 160)) {
-			$('section').find('.title').removeClass('stuck');
-			$currentTitle.removeClass('stuck');
-			$currentTitle.addClass('bottom');
-		}
-		else if ($scroll < ($currentBottom - $(window).height() + 160) && $scroll >= ($currentTop - 160)) {
-			$('section').find('.title').removeClass('stuck');
-			$currentTitle.removeClass('bottom');
-			$currentTitle.addClass('stuck');
-		}
-		// ---------------------------------------------------------------------
-		// show and hide logo (right rail), scrolling from hero
-		var $missionTop = $('.mission-section').offset().top;
-		if ($scroll > ($missionTop - .6*$(window).height())) {
-			$('.logo').removeClass('out');
-		}
-		else if ($scroll < ($missionTop - .6*$(window).height())) {
-			$('.logo').addClass('out');
-		}
-		// ---------------------------------------------------------------------
 		// show and hide arrows (bottom left)
 		var $footerTop = $('.footer').offset().top;
 		if ($scroll > ($footerTop - $(window).height())) {
-			$('.arrow').addClass('hidden');
+			$('.arrow-down').addClass('hidden');
 		}
 		else if ($scroll < ($footerTop - $(window).height())) {
-			$('.arrow').removeClass('hidden');
+			$('.arrow-down').removeClass('hidden');
 		}
 	});
-
 });
