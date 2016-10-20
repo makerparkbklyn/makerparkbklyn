4c4784ab53328e1366c1f9933a20f4a02fe80c88
mattarnoldpentagram <matt.arnold@pentagram.com>
2016-10-20 11:19:44 -0400

    first pass bg color fading works but transition not controlled by scroll

--- js/site.js
+++ js/site.js
@@ -60,6 +60,10 @@ $(document).on('ready', function() {
 	var $currentSection = $('section.current');
 	var $prevSection = null;
 	var lastScrollTop = 0;
+	var currentBgColor = null;
+
+	// -------------------------------------------------------------------------
+	// animate colors
 
 	$(window).scroll(function() {
 
@@ -70,6 +74,7 @@ $(document).on('ready', function() {
 			var $currentTop = $currentSection.offset().top;
 			var $currentBottom = $currentSection.offset().top + $currentSection.outerHeight();
 			var $currentTitle = $currentSection.find('.title');
+
 			// -----------------------------------------------------------------
 			// set currentSection
 			if ($currentSection.next('section').length > 0) {
@@ -89,15 +94,16 @@ $(document).on('ready', function() {
 				$currentBottom = $currentSection.offset().top + $currentSection.outerHeight();
 				$currentTitle = $currentSection.find('.title');
 				$('section.current').removeClass('current');
-				$currentSection.addClass('current');
 			}
+
+			fadeBg($scroll);
+
 			// -----------------------------------------------------------------
 			// make titles stick
 			if ($scroll >= ($currentTop - 160)) {
 				$('section').find('.title').removeClass('stuck');
 				$currentTitle.removeClass('top');
 				$currentTitle.addClass('stuck');
-
 			} else if ($scroll < ($currentTop - 160)) {
 				$('section').find('.title').removeClass('stuck');
 				$currentTitle.removeClass('stuck');
@@ -115,11 +121,10 @@ $(document).on('ready', function() {
 			}
 			// -----------------------------------------------------------------
 			// show and hide logo (right rail), scrolling from hero
-			var $missionTop = $('.mission-section').offset().top;
-			if ($scroll > ($missionTop - .6*$(window).height())) {
+			if ( $scroll > ($(window).height() * 1.4) ) {
 				$('.logo').removeClass('out');
 			}
-			else if ($scroll < ($missionTop - .6*$(window).height())) {
+			else if ( $scroll < ($(window).height() * 1.4) ) {
 				$('.logo').addClass('out');
 			}
 		}
@@ -128,14 +133,12 @@ $(document).on('ready', function() {
 		var st = this.pageYOffset || this.scrollTop;
 		if (st > lastScrollTop){
 			// downscroll code
-			console.log('scolling down...');
 			if ($(window).width() < 1280) {
 				$('.rail').addClass('out');
 			}
 			$('.nav').removeClass('open');
 		} else {
 			// upscroll code
-			console.log('scolling up...');
 			$('.rail').removeClass('out');
 		}
 		lastScrollTop = st;
@@ -149,4 +152,78 @@ $(document).on('ready', function() {
 			$('.arrow-down').removeClass('hidden');
 		}
 	});
+
+	// -------------------------------------------------------------------------
+	// background color transitions FUNCTION
+	var prevColoredSection = null;
+	function fadeBg($scroll) {
+		var bgAnimationSpeed = 300;
+		if (prevColoredSection != $currentSection) {
+			switch($currentSection.attr('id')) {
+				case 'mission':
+					if (prevColoredSection != null && prevColoredSection.attr('id') === 'about') {
+						$('.bg').animate({
+							backgroundColor: 'rgb(216, 242, 130)'
+						}, bgAnimationSpeed);
+					}
+					break;
+				case 'about':
+					$('.bg').animate({
+						backgroundColor: 'rgb(152, 240, 200)'
+					}, bgAnimationSpeed);
+					break;
+				case 'vision':
+					$('.bg').animate({
+						backgroundColor: 'rgb(145, 230, 223)'
+					}, bgAnimationSpeed);
+					break;
+				case 'timeline':
+					$('.bg').animate({
+						backgroundColor: 'rgb(168, 197, 255)'
+					}, bgAnimationSpeed);
+					break;
+				case 'principles':
+					$('.bg').animate({
+						backgroundColor: 'rgb(200, 176, 255)'
+					}, bgAnimationSpeed);
+					break;
+				case 'join':
+					$('.bg').animate({
+						backgroundColor: 'rgb(255, 148, 189)'
+					}, bgAnimationSpeed);
+					break;
+				case 'news':
+					$('.bg').animate({
+						backgroundColor: 'rgb(255, 125, 129)'
+					}, bgAnimationSpeed);
+					break;
+				case 'team':
+					$('.bg').animate({
+						backgroundColor: 'rgb(255, 128, 83)'
+					}, bgAnimationSpeed);
+					break;
+			}
+		}
+		// mission bg transitions
+		if ($scroll > $(window).height() && $scroll < ($(window).height()*1.5) ) {
+			$('.bg').css('background-color', 'rgb(216, 242, 130)');
+			$('.mission-section').css('background-color', 'transparent');
+		}
+		else if ($scroll < $(window).height()) {
+			$('.mission-section').css('background-color', 'rgb(216, 242, 130)');
+			$('.bg').css('background-color', 'transparent');
+		}
+		// footer bg transitions
+		var footerTop = $('.footer').offset().top;
+		var teamTop = $('.team-section').offset().top;
+		if ($scroll > (footerTop - $(window).height()) ) {
+			$('.team-section').css('background-color', 'rgb(255, 128, 83)');
+			$('.bg').css('background-color', 'transparent');
+		}
+		else if ($scroll < (footerTop - $(window).height()) && $scroll > teamTop ) {
+			$('.team-section').css('background-color', 'transparent');
+			$('.bg').css('background-color', 'rgb(255, 128, 83)');
+		}
+		prevColoredSection = $currentSection;
+	}
 });
