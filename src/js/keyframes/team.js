435291137e3e96d5f05b76f15624a673f422f18b
Austin Maurer <r.austin.maurer@gmail.com>
2018-06-04 13:41:16 -0400

    updates from MP team - remove team photo, fix typo, etc

--- src/js/keyframes/team.js
+++ src/js/keyframes/team.js
@@ -26,168 +26,168 @@ const teamKeyframes = () => {
 			{
 				name	:	'titleIn',
 				duration:	1.4 * wh,
-				offset	:	0,
+				offset	:	0.2 * wh,
 				element :	'#team .section__title-xl',
 				tween	:	{y: wh, ease: Power2.easeOut}
 			},
 			{
 				name	:	'titleOut',
 				duration:	1.4 * wh,
-				offset	:	1.75 * wh,
+				offset	:	2.2 * wh,
 				element :	'#team .section__title-xl',
 				tween	: 	{y: -wh, ease: Power2.easeIn}
 			},
-			{
-				name	:	'teamPhotoIn',
-				duration:	1.4 * wh,
-				offset	:	.25 * wh,
-				element :	'#team .team-photo',
-				tween	: 	{y: wh, x: -800, ease: Power2.easeOut}
-			},
-			{
-				name	:	'teamPhotoOut',
-				duration:	1.4 * wh,
-				offset	:	1.75 * wh,
-				element :	'#team .team-photo',
-				tween	: 	{y: -wh, x: 800, ease: Power2.easeIn}
-			},
-			{
-				name	:	'pattern1In',
-				duration:	1.4 * wh,
-				offset	:	0.25 * wh,
-				element :	'#team .bg-pattern-1',
-				tween	: 	{y: 1.5 * wh, ease: Power2.easeOut}
-			},
-			{
-				name	:	'pattern1Out',
-				duration:	1.4 * wh,
-				offset	:	2 * wh,
-				element :	'#team .bg-pattern-1',
-				tween	: 	{y: -2 * wh, x: 600, ease: Power2.easeIn}
-			},
+			// {
+			// 	name	:	'teamPhotoIn',
+			// 	duration:	1.4 * wh,
+			// 	offset	:	.25 * wh,
+			// 	element :	'#team .team-photo',
+			// 	tween	: 	{y: wh, x: -800, ease: Power2.easeOut}
+			// },
+			// {
+			// 	name	:	'teamPhotoOut',
+			// 	duration:	1.4 * wh,
+			// 	offset	:	1.75 * wh,
+			// 	element :	'#team .team-photo',
+			// 	tween	: 	{y: -wh, x: 800, ease: Power2.easeIn}
+			// },
+			// {
+			// 	name	:	'pattern1In',
+			// 	duration:	1.4 * wh,
+			// 	offset	:	0.25 * wh,
+			// 	element :	'#team .bg-pattern-1',
+			// 	tween	: 	{y: 1.5 * wh, ease: Power2.easeOut}
+			// },
+			// {
+			// 	name	:	'pattern1Out',
+			// 	duration:	1.4 * wh,
+			// 	offset	:	2 * wh,
+			// 	element :	'#team .bg-pattern-1',
+			// 	tween	: 	{y: -2 * wh, x: 600, ease: Power2.easeIn}
+			// },
 			{
 				name	:	'expertLogo1In',
 				duration:	1.4 * wh,
-				offset	:	2.6 * wh,
+				offset	:	0.6 * wh,
 				element :	'#team .expert-team-block:nth-child(1)',
 				tween	:	{y: wh, ease: Power2.easeOut}
 			},
 			{
 				name	:	'expertLogo2In',
 				duration:	1.4 * wh,
-				offset	:	2.65 * wh,
+				offset	:	0.65 * wh,
 				element : 	'#team .expert-team-block:nth-child(2)',
 				tween	:	{y: wh, ease: Power2.easeOut}
 			},
 			{
 				name	:	'expertLogo3In',
 				duration:	1.4 * wh,
-				offset	:	2.7 * wh,
+				offset	:	0.7 * wh,
 				element :	'#team .expert-team-block:nth-child(3)',
 				tween	:	{y: wh, ease: Power2.easeOut}
 			},
 			{
 				name	:	'expertLogo4In',
 				duration:	1.4 * wh,
-				offset	:	2.75 * wh,
+				offset	:	0.75 * wh,
 				element :	'#team .expert-team-block:nth-child(4)',
 				tween	:	{y: wh, ease: Power2.easeOut}
 			},
 			{
 				name	:	'expertLogo5In',
 				duration:	1.4 * wh,
-				offset	:	2.8 * wh,
+				offset	:	0.8 * wh,
 				element :	'#team .expert-team-block:nth-child(5)',
 				tween	:	{y: wh, ease: Power2.easeOut}
 			},
 			{
 				name	:	'expertLogo6In',
 				duration:	1.4 * wh,
-				offset	:	2.85 * wh,
+				offset	:	0.85 * wh,
 				element :	'#team .expert-team-block:nth-child(6)',
 				tween	:	{y: wh, ease: Power2.easeOut}
 			},
 			{
 				name	:	'expertLogo7In',
 				duration:	1.4 * wh,
-				offset	:	2.9 * wh,
+				offset	:	0.9 * wh,
 				element :	'#team .expert-team-block:nth-child(7)',
 				tween	:	{y: wh, ease: Power2.easeOut}
 			},
 			{
 				name	:	'expertLogo8In',
 				duration:	1.4 * wh,
-				offset	:	2.95 * wh,
+				offset	:	0.95 * wh,
 				element :	'#team .expert-team-block:nth-child(8)',
 				tween	:	{y: wh, ease: Power2.easeOut}
 			},
 			{
 				name	:	'expertLogo9In',
 				duration:	1.4 * wh,
-				offset	:	3 * wh,
+				offset	:	1 * wh,
 				element :	'#team .expert-team-block:nth-child(9)',
 				tween	:	{y: wh, ease: Power2.easeOut}
 			},
 			{
 				name	:	'expertLogo1Out',
 				duration:	1.4 * wh,
-				offset	:	4.5 * wh,
+				offset	:	2.5 * wh,
 				element :	'#team .expert-team-block:nth-child(1)',
 				tween	:	{y: -wh, ease: Power2.easeIn}
 			},
 			{
 				name	:	'expertLogo2Out',
 				duration:	1.4 * wh,
-				offset	:	4.6 * wh,
+				offset	:	2.6 * wh,
 				element :	'#team .expert-team-block:nth-child(2)',
 				tween	:	{y: -wh, ease: Power2.easeIn}
 			},
 			{
 				name	:	'expertLogo3Out',
 				duration:	1.4 * wh,
-				offset	:	4.7 * wh,
+				offset	:	2.7 * wh,
 				element :	'#team .expert-team-block:nth-child(3)',
 				tween	:	{y: -wh, ease: Power2.easeIn}
 			},
 			{
 				name	:	'expertLogo4Out',
 				duration:	1.4 * wh,
-				offset	:	4.8 * wh,
+				offset	:	2.8 * wh,
 				element :	'#team .expert-team-block:nth-child(4)',
 				tween	:	{y: -wh, ease: Power2.easeIn}
 			},
 			{
 				name	:	'expertLogo5Out',
 				duration:	1.4 * wh,
-				offset	:	4.9 * wh,
+				offset	:	2.9 * wh,
 				element :	'#team .expert-team-block:nth-child(5)',
 				tween	:	{y: -wh, ease: Power2.easeIn}
 			},
 			{
 				name	:	'expertLogo6Out',
 				duration:	1.4 * wh,
-				offset	:	5 * wh,
+				offset	:	3 * wh,
 				element :	'#team .expert-team-block:nth-child(6)',
 				tween	:	{y: -wh, ease: Power2.easeIn}
 			},
 			{
 				name	:	'expertLogo7Out',
 				duration:	1.4 * wh,
-				offset	:	5.1 * wh,
+				offset	:	3.1 * wh,
 				element :	'#team .expert-team-block:nth-child(7)',
 				tween	:	{y: -wh, ease: Power2.easeIn}
 			},
 			{
 				name	:	'expertLogo8Out',
 				duration:	1.4 * wh,
-				offset	:	5.2 * wh,
+				offset	:	3.2 * wh,
 				element :	'#team .expert-team-block:nth-child(8)',
 				tween	:	{y: -wh, ease: Power2.easeIn}
 			},
 			{
 				name	:	'expertLogo9Out',
 				duration:	1.4 * wh,
-				offset	:	5.3 * wh,
+				offset	:	3.3 * wh,
 				element :	'#team .expert-team-block:nth-child(9)',
 				tween	:	{y: -wh, ease: Power2.easeIn}
 			},
